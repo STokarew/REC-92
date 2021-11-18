81c4edc9a3fa965cb67817504e7c96ca9c4e6ff4
сергей токарев <sseregatokarew@gmail.com>
2021-11-18 14:46:48 +0700

    Добавьте файлы проекта.

---
+++ REC-92/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace REC_92
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите натуральное число: ");
+            var NaturalNum = Console.ReadLine();
+            Console.Write("Введите искомую цыфру: ");
+            var Num = Console.ReadLine();
+            var CountNum = NaturalNum.Count(c => c == char.Parse(Num));
+            Console.WriteLine(string.Format($"Цыфра {Num} встречается {CountNum} раз"));
+        }
+    }
+}
